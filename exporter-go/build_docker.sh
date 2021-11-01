f1c80d965c61f4ab17984e48b07457f8a1889022
Lee Mosemann <59905066+lmoze@users.noreply.github.com>
2021-11-01 10:27:22 -0600

    Add authentication docs note in build_docker.sh

--- exporter-go/build_docker.sh
+++ exporter-go/build_docker.sh
@@ -4,4 +4,7 @@ IMAGE=${1:-paytmlabs/opa_scorecard_exporter:v0.0.4-labs-0.0.1}
 
 
 docker build --tag="${IMAGE}" .
+
+# Push will fail unless docker is logged in to a Github account with write access to the Packages repo
+# See https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry#authenticating-to-the-container-registry
 docker push "${IMAGE}"
