367df6b6b585b41496ab8408ec7d30f924a0613e
Lee Mosemann <59905066+lmoze@users.noreply.github.com>
2021-11-01 10:30:25 -0600

    Fix docker image tag to match Github Packages repo

--- exporter-go/build_docker.sh
+++ exporter-go/build_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE=${1:-paytmlabs/opa_scorecard_exporter:v0.0.4-labs-0.0.1}
+IMAGE=${1:-ghcr.io/paytmlabs/opa_scorecard_exporter:v0.0.4-labs-0.0.1}
 
 
 docker build --tag="${IMAGE}" .
