30497f0a1ae6b12b0b54c444df7a6a44c0418340
Lee Mosemann <59905066+lmoze@users.noreply.github.com>
2021-11-04 16:58:11 -0600

    PaytmLabs Docker Build to Github Packages (#2)
    * Modify build scripts for PaytmLabs build to Github Packages
    
    * Add authentication docs note in build_docker.sh
    
    * Fix docker image tag to match Github Packages repo

--- exporter-go/build_docker.sh
+++ exporter-go/build_docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-IMAGE=${1:-mcelep/opa_scorecard_exporter:v0.0.3}
+IMAGE=${1:-ghcr.io/paytmlabs/opa_scorecard_exporter:v0.0.4-labs-0.0.1}
 
 
 docker build --tag="${IMAGE}" .
+
+# Push will fail unless docker is logged in to a Github account with write access to the Packages repo
+# See https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry#authenticating-to-the-container-registry
 docker push "${IMAGE}"
